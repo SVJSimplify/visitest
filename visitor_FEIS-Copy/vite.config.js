41ac2ab2c85992124a527150dc112aa9a7ce7e8a
SVJSimplify <Reach.Vikhyath@gmail.com>
2026-04-14 11:27:46 +0530

    Update vite.config.js

--- visitor_FEIS-Copy/vite.config.js
+++ visitor_FEIS-Copy/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173, host: true },
+  server: { port: 5500, host: true },
   build: {
     outDir: 'dist',
     sourcemap: false,
